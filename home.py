a96a073a7a3b73fa9f22a3d7a90789e9e792d520
Ed Saa <esaavedrac@u.northwestern.edu>
2022-08-26 11:36:18 -0500

    basic layout

---
+++ home.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Process ADV data")
+
+st.header("Processing a single point")
---
+++ pages/01_Process_Point.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import plotly
+
+"# **Data processing for turbulence analysis (1/3)**"
+
+"##  Read netCDF4 file from Vectrino Profiler"
