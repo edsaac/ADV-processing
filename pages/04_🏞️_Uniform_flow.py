3029cb98fd78b075d89f8b709a979d1462eb6de4
Edwin <esaavedrac@u.northwestern.edu>
2022-08-31 19:34:12 -0500

    add assets

--- pages/04_🏞️_Uniform_flow.py
+++ pages/04_🏞️_Uniform_flow.py
@@ -11,46 +11,88 @@ r"""
 # 🏞️ **Uniform flow characterization**
 
 ## Channel dimensions
+"""
 
-The **depth** $y$ of the channel was measured in the lab, whereas the width 
-is $L_w = 0.29 \, \mathrm{m}$ for the flume used. 
+cols = st.columns([2,1,1])
 
-For a rectangular channel, the **cross-sectional** area $A_C$ and the 
-**wetted perimeter** $P_W$ are given by
+with cols[0]:
+    r"""
 
-$$
-    A_C = L_w \cdot y
-$$
+    The **depth** $y$ of the channel was measured in the lab, whereas the width 
+    is $L_w = 0.29 \, \mathrm{m}$ for the flume used. 
 
-$$
-    P_W = 2y + L_w
-$$
+    For a rectangular channel, the **cross-sectional** area $A_C$ and the 
+    **wetted perimeter** $P_W$ are given by
 
-The ratio between these two quantities give the **hydraulic radius** $R_H$
+    $$
+        A_C = L_w \cdot y
+    $$
 
-$$
-    R_H = \dfrac{A_C}{P_W}
-$$
+    $$
+        P_W = 2y + L_w
+    $$
+    """
 
-Finally, the channel slope $S_0$ was measured during the lab.
+    """
+    The ratio between these two quantities give the **hydraulic radius** $R_H$
 
+    $$
+        R_H = \dfrac{A_C}{P_W}
+    $$
+
+    Finally, the channel slope $S_f$ was measured during the lab. 
+    """
+
+    st.warning("How is $S_f$ calculated from those readings?")
+
+with cols[1]:
+    st.image("./assets/widthchannel.jpg",
+        use_column_width=True,
+        caption="Channel width")
+
+with cols[2]:
+    st.image("./assets/slopemeter.jpg",
+        use_column_width=True,
+        caption="Measuring the slope of the channel.")
+
+"""
+****
 ## Flow parameters
+"""
 
-The volumetric **flow rate** $Q$ was measured with the flowmeter in the 
-return pipe of the channel. The **bulk velocity** $U$
+col1, col2 = st.columns([2,1])
 
-$$
-    U = \dfrac{Q}{A_C}
-$$
+with col1:
+    r"""
+    The volumetric **flow rate** $Q$ was measured with the flowmeter in the 
+    return pipe of the channel. Use these readings to calculate the 
+    **bulk velocity** $U$:
 
-The Reynolds number $\mathcal{R}$ for a channel is defined as
+    $$
+        U = \dfrac{Q}{A_C}
+    $$
 
-$$
-    \mathcal{R} = \dfrac{4 U R_H}{\nu}
-$$
+    The Reynolds number $\mathcal{R}$ for a channel is defined as
 
-Where \nu is the kinematic viscosity of water
+    $$
+        \mathcal{R} = \dfrac{4 U R_H}{\nu}
+    $$
 
+    Where $\nu$ is the kinematic viscosity of water
+    """
+
+    st.info(
+        """
+        Was the flow in the channel *turbulent*? 
+        Confirm this with the Reynolds number
+        """ )
+
+with col2:
+    st.image("./assets/flowmeter.jpg",use_column_width=True,
+        caption="Volumetric flowmeter.")
+
+r"""
+****
 ## Shear stress
 
 The **shear stress** $\tau_0$ at the bottom of the channel is given by
@@ -65,6 +107,24 @@ The shear velocity $u^*$ is:
 $$
     u^* = \sqrt{\dfrac{\tau_0}{\rho}}
 $$
+"""
+
+st.info(r"What are the units of $\tau_0$ and $u^*$?")
+
+r"""
+The thickness of the viscous (a.k.a. laminar) sublayer $\delta_v$ can be 
+approximated to
+
+$$
+    \delta_v = 
+$$
+
+"""
+
+r"""
+****
+
+## Friction factor
 
 The Darcy-Weisbach equation for open channel flow is 
 
@@ -73,8 +133,12 @@ $$
 $$
 
 where $f$ is the friction factor and $S_f$ is the energy line slope, which, for
-uniform flow is assumed to be equal to the channel slope $S_0$
+uniform flow is assumed to be equal to the bottom slope $S_0$
+"""
 
+st.info("Solve for $f$ to obtain the friction factor. What are the units of $f$?")
+
+r"""
 Semi-empirical relationships exist between the Reynolds number, the friction
 factor and the bottom roughness $k_s$, for instance,
 
@@ -82,8 +146,11 @@ $$
     \dfrac{1}{\sqrt{f}} = -2 \log{\left( \dfrac{k_s}{12R_H} + \dfrac{2.5}{\mathcal{R}\sqrt{f}} \right)}
 $$
 
-### Boundary layer
-
-The thickness of the laminar sublayer $\delta$
-
 """
+
+st.info(
+    """
+    - Check that the units on this equation are consistent
+    - Solve for $k_s$.
+    - Is the result close to your roughness measurement?"""
+    )
